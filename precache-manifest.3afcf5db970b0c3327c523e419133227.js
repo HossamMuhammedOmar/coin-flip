8422627023861cb443302689ca18e7fefee8b127
HossamMuhammedOmar <hossamomar9876@gmail.com>
2020-09-07 06:30:53 +0200

    Updates

---
+++ precache-manifest.3afcf5db970b0c3327c523e419133227.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6715d8dc2a2415952a2b42a6dd99090",
+    "url": "/coin-flip/index.html"
+  },
+  {
+    "revision": "5731fed03afee5da9168",
+    "url": "/coin-flip/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "03c57e278b94835e5c64",
+    "url": "/coin-flip/static/js/2.5fb46d68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coin-flip/static/js/2.5fb46d68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5731fed03afee5da9168",
+    "url": "/coin-flip/static/js/main.676d2d38.chunk.js"
+  },
+  {
+    "revision": "e8b70a576d0b3f69541d",
+    "url": "/coin-flip/static/js/runtime-main.7843c0ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coin-flip/static/media/logo.5d5d9eef.svg"
+  }
+]);
